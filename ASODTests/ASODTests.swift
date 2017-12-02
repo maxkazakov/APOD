becfc2fe1e75ec330e0d05267c2a7f28cec95642
Max Kazakov <kazakov.maxim.a@gmail.com>
2017-12-02 15:32:36 +0500

    network test

--- ASODTests/ASODTests.swift
+++ ASODTests/ASODTests.swift
@@ -37,31 +37,49 @@ class ASODTests: XCTestCase {
     }
     """
     
-    let pictureModel = PictureModel(value: ["date" : "2011-12-28",
-                                           "url": "https://apod.nasa.gov/apod/image/1112/IMG_9800-GBLANCHARD900.jpg",
-                                           "explanation": "Comet Lovejoy (C/2011 W3) survived its close encounter with the Sun earlier this month, taking its place among wonders of the southern skies just in time for Christmas. Seen here before sunrise from Paranal Observatory in Chile, the sungrazing comet's tails stretch far above the eastern horizon. Spanning over 20 degrees they rise alongside the plane of the our Milky Way galaxy. A breathtaking spectacle in itself, Lovejoy performs on this celestial stage with southern stars and nebulae, including the Large and Small Magellanic clouds right of the telescope dome, and the glow of zodiacal light along the left edge of the frame. With Paranal's Very Large Telescope units in the foreground, this wide-angle scene was captured on December 23. Receding from the Sun, Comet Lovejoy's tails have continued to grow in length even as the comet fades.",
-                                           "media_type": "image",
-                                           "title": "Comet Lovejoy over Paranal",
-                                           "hdurl": "https://apod.nasa.gov/apod/image/1112/IMG_9800-GBLANCHARD.jpg",
-                                           "copyright": "Guillaume Blanchard"])
-        
+//    let pictureModel = PictureModel(value: ["date" : "2011-12-28",
+//                                           "url": "https://apod.nasa.gov/apod/image/1112/IMG_9800-GBLANCHARD900.jpg",
+//                                           "explanation": "Comet Lovejoy (C/2011 W3) survived its close encounter with the Sun earlier this month, taking its place among wonders of the southern skies just in time for Christmas. Seen here before sunrise from Paranal Observatory in Chile, the sungrazing comet's tails stretch far above the eastern horizon. Spanning over 20 degrees they rise alongside the plane of the our Milky Way galaxy. A breathtaking spectacle in itself, Lovejoy performs on this celestial stage with southern stars and nebulae, including the Large and Small Magellanic clouds right of the telescope dome, and the glow of zodiacal light along the left edge of the frame. With Paranal's Very Large Telescope units in the foreground, this wide-angle scene was captured on December 23. Receding from the Sun, Comet Lovejoy's tails have continued to grow in length even as the comet fades.",
+//                                           "media_type": "image",
+//                                           "title": "Comet Lovejoy over Paranal",
+//                                           "hdurl": "https://apod.nasa.gov/apod/image/1112/IMG_9800-GBLANCHARD.jpg",
+//                                           "copyright": "Guillaume Blanchard"])
+    
     
     
     
     func testPictureModel() {
-        let picture = PictureModel(from: pictureJsonString)
+        let dict = PictureModel(value: ["date" : dateFormatter.date(from: "2011-11-11")!,
+                                                "url": "https://apod.nasa.gov/apod/image/1112/IMG_9800-GBLANCHARD900.jpg",
+                                                "explanation": "Comet Lovejoy (C/2011 W3) survived its close encounter with the Sun earlier this month, taking its place among wonders of the southern skies just in time for Christmas. Seen here before sunrise from Paranal Observatory in Chile, the sungrazing comet's tails stretch far above the eastern horizon. Spanning over 20 degrees they rise alongside the plane of the our Milky Way galaxy. A breathtaking spectacle in itself, Lovejoy performs on this celestial stage with southern stars and nebulae, including the Large and Small Magellanic clouds right of the telescope dome, and the glow of zodiacal light along the left edge of the frame. With Paranal's Very Large Telescope units in the foreground, this wide-angle scene was captured on December 23. Receding from the Sun, Comet Lovejoy's tails have continued to grow in length even as the comet fades.",
+                                                "media_type": "image",
+                                                "title": "Comet Lovejoy over Paranal",
+                                                "hdurl": "https://apod.nasa.gov/apod/image/1112/IMG_9800-GBLANCHARD.jpg",
+                                                "copyright": "Guillaume Blanchard"])
         
-        XCTAssert(picture != nil)
-        XCTAssertEqual(picture!.date, pictureModel.date)
-        XCTAssertEqual(picture!.url, pictureModel.url)
-        XCTAssertEqual(picture!.explanation, pictureModel.explanation)
-        XCTAssertEqual(picture!.media_type, pictureModel.media_type)
-        XCTAssertEqual(picture!.title, pictureModel.title)
-        XCTAssertEqual(picture!.hdurl, pictureModel.hdurl)
-        XCTAssertEqual(picture!.copyright, pictureModel.copyright)
+        let picture = PictureModel(value: dict)
+        
+        XCTAssertEqual(picture.date, dateFormatter.date(from: "2011-11-11"))
+        XCTAssertEqual(picture.url, "https://apod.nasa.gov/apod/image/1112/IMG_9800-GBLANCHARD900.jpg")
+        XCTAssertEqual(picture.explanation, "Comet Lovejoy (C/2011 W3) survived its close encounter with the Sun earlier this month, taking its place among wonders of the southern skies just in time for Christmas. Seen here before sunrise from Paranal Observatory in Chile, the sungrazing comet's tails stretch far above the eastern horizon. Spanning over 20 degrees they rise alongside the plane of the our Milky Way galaxy. A breathtaking spectacle in itself, Lovejoy performs on this celestial stage with southern stars and nebulae, including the Large and Small Magellanic clouds right of the telescope dome, and the glow of zodiacal light along the left edge of the frame. With Paranal's Very Large Telescope units in the foreground, this wide-angle scene was captured on December 23. Receding from the Sun, Comet Lovejoy's tails have continued to grow in length even as the comet fades.")
+        XCTAssertEqual(picture.media_type, "image")
+        XCTAssertEqual(picture.title, "Comet Lovejoy over Paranal")
+        XCTAssertEqual(picture.hdurl, "https://apod.nasa.gov/apod/image/1112/IMG_9800-GBLANCHARD.jpg")
+        XCTAssertEqual(picture.copyright, "Guillaume Blanchard")
     }
     
     
+    
+    func testNetwork() {
+        let testDates = ["2017-11-11"/*, "2011-11-11", "2015-11-11"*/].map{ dateFormatter.date(from: $0)! }
+        do {
+            let pictures = try NetworkApiService.loadPictures(from: testDates)
+            XCTAssertEqual(pictures.count, 3)
+        }
+        catch {
+            XCTFail()
+        }
+    }
     
     
     func testPerformanceExample() {
